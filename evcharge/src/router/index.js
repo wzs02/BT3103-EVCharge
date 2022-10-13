bf2184c010239c08340109fc5e33a130231579d2
Nat-Han1999 <88693280+Nat-Han1999@users.noreply.github.com>
2022-10-13 22:14:23 +0800

    Add index.js for router

---
+++ evcharge/src/router/index.js
@@ -0,0 +1,14 @@
+import { createRouter, createWebHashHistory, createWebHistory } from "vue-router"
+
+
+
+const routes = [
+    
+]
+
+const router = createRouter({
+    history: createWebHashHistory(),
+    routes
+})
+
+export default router
