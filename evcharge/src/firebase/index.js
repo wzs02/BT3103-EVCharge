43b5af64c3be6eace645fe4f2205b12f7cd7b9b1
Nat-Han1999 <88693280+Nat-Han1999@users.noreply.github.com>
2022-10-14 15:28:49 +0800

    Transferred Firebase to main.js

---
+++ evcharge/src/firebase/index.js
@@ -0,0 +1,23 @@
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getAuth } from "firebase/auth";
+
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBX-FPGba8n4lhgzxAYsUQ-txrXaDA5ACI",
+//   authDomain: "evcharge-firebase.firebaseapp.com",
+//   projectId: "evcharge-firebase",
+//   storageBucket: "evcharge-firebase.appspot.com",
+//   messagingSenderId: "168097413066",
+//   appId: "1:168097413066:web:ba9a178747002a64504133"
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+
+// const auth = getAuth(app);
+
+// export { auth }
