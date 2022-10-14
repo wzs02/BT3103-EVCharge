d1f52e86d6e3f6b01a5cd2d70a1415ac0d26d1d2
Nat-Han1999 <88693280+Nat-Han1999@users.noreply.github.com>
2022-10-14 15:27:41 +0800

    insert firebaseconfig into main.js

--- evcharge/src/main.js
+++ evcharge/src/main.js
@@ -2,9 +2,30 @@ import 'vuetify/styles' // Global CSS has to be imported
 import { createApp } from 'vue'
 import { createVuetify } from 'vuetify'
 import App from './App.vue'
+import router from './router'
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBX-FPGba8n4lhgzxAYsUQ-txrXaDA5ACI",
+  authDomain: "evcharge-firebase.firebaseapp.com",
+  projectId: "evcharge-firebase",
+  storageBucket: "evcharge-firebase.appspot.com",
+  messagingSenderId: "168097413066",
+  appId: "1:168097413066:web:ba9a178747002a64504133"
+};
+
+initializeApp(firebaseConfig);
 
 const app = createApp(App)
+
 const vuetify = createVuetify() // Replaces new Vuetify()
+
+app.use(router)
 
 app.use(vuetify)
 
