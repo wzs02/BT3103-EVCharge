25dd632e7b50fd956540994cc04336bfe0b46111
Nat-Han1999 <88693280+Nat-Han1999@users.noreply.github.com>
2022-10-15 13:12:50 +0800

    Add google auth

--- evcharge/src/router/index.js
+++ evcharge/src/router/index.js
@@ -1,59 +1,35 @@
 import { createRouter, createWebHashHistory } from "vue-router"
-import { getAuth, onAuthStateChanged} from "firebase/auth"
+// import LogIn from '@/views/LogIn.vue'
+// import SignUp from '@/views/SignUp.vue'
+// import TesterFile from '@/views/TesterFile.vue'
 
 const router = createRouter({
-    history: createWebHashHistory(),
-    routes: [
+  history: createWebHashHistory(),
+  routes: [
     {
-        path: '/',
-        name: 'AboutPage',
-        component: () => import('../views/AboutPage.vue'),
+      path: "/",
+      name: "AboutPage",
+      component: () => import("../views/AboutPage.vue"),
     },
     {
-        path: '/signup',
-        name:'SignUp',
-        component: () => import('../views/SignUp.vue'),
+      path: "/signup",
+      name: "SignUp",
+      component: () => import("../views/SignUp.vue"),
     },
     {
-        path: '/login',
-        name:'LogIn',
-        component: () => import('../views/LogIn.vue'),
+      path: "/login",
+      name: "LogIn",
+      component: () => import("../views/LogIn.vue"),
     },
     {
         path: '/TesterFile',
         name:'TesterFile',
         component: () => import('../views/TesterFile.vue'),
         meta: {
-            requiresAuth: true,
+            requireAuth: true
         }
     }
 ],
 })
 
-const getCurrentUser = () => {
-    return new Promise((resolve) => {
-        const removeListener = onAuthStateChanged(
-            getAuth(),
-            (user) => {
-                removeListener();
-                resolve(user);
-            }
-        )
-    })
-};
-
-
-router.beforeEach(async (to, from ,next) => {
-    if (to.matched.some((record) => record.meta.requiresAuth)) {
-        if (await getCurrentUser()) {
-            next();
-        } else{
-            alert("You are not signed in, please create an account!");
-            next("/signup");
-        }
-    } else {
-        next();
-    }
-});
-
-export default router;
+export default router
