c6df556eb555f092b2a0b321e6d57df668843175
laitzheng <75247468+laitzheng@users.noreply.github.com>
2022-10-15 16:47:04 +0800

    Create view_bookings page

--- evcharge/src/router/index.js
+++ evcharge/src/router/index.js
@@ -1,35 +1,43 @@
-import { createRouter, createWebHashHistory } from "vue-router"
+import { createRouter, createWebHashHistory } from "vue-router";
 // import LogIn from '@/views/LogIn.vue'
 // import SignUp from '@/views/SignUp.vue'
 // import TesterFile from '@/views/TesterFile.vue'
 
 const router = createRouter({
-    history: createWebHashHistory(),
-    routes: [
-    {
-        path: '/',
-        name: 'AboutPage',
-        component: () => import('../views/AboutPage.vue'),
-    },
-    {
-        path: '/signup',
-        name:'SignUp',
-        component: () => import('../views/SignUp.vue'),
-    },
-    {
-        path: '/login',
-        name:'LogIn',
-        component: () => import('../views/LogIn.vue'),
-    },
-    {
-        path: '/TesterFile',
-        name:'TesterFile',
-        component: () => import('../views/TesterFile.vue'),
-        meta: {
-            requireAuth: true
-        }
-    }
-],
-})
+	history: createWebHashHistory(),
+	routes: [
+		{
+			path: "/",
+			name: "AboutPage",
+			component: () => import("../views/AboutPage.vue"),
+		},
+		{
+			path: "/signup",
+			name: "SignUp",
+			component: () => import("../views/SignUp.vue"),
+		},
+		{
+			path: "/login",
+			name: "LogIn",
+			component: () => import("../views/LogIn.vue"),
+		},
+		{
+			path: "/view_bookings",
+			name: "ViewBookings",
+			component: () => import("../views/ViewBookings.vue"),
+			meta: {
+				requireAuth: true,
+			},
+		},
+		{
+			path: "/TesterFile",
+			name: "TesterFile",
+			component: () => import("../views/TesterFile.vue"),
+			meta: {
+				requireAuth: true,
+			},
+		},
+	],
+});
 
-export default router
+export default router;
---
+++ evcharge/src/views/ViewBookings.vue
@@ -0,0 +1,47 @@
+<template>
+  <v-app>
+    <NavBar />
+    <div id="view_bookings">
+      <h1 id="view_bookings_header">Bookings</h1>
+      <p id="view_bookings_upcoming">Upcoming</p>
+      <p id="view_bookings_past">Past</p>
+    </div>
+  </v-app>
+</template>
+
+<script>
+import NavBar from "../components/NavBar.vue";
+
+export default {
+    components: {NavBar}
+
+}
+</script>
+
+<style scoped>
+#view_bookings {
+  margin-top: 170px;
+  margin-left: 137px;
+}
+
+#view_bookings_header {
+  font-family: 'Outfit', sans-serif;
+  font-weight: bold;
+  font-size: 70px;
+}
+
+#view_bookings_upcoming{
+  margin-top: 115px;
+  font-family: 'Outfit', sans-serif;
+  font-weight: 400;
+  font-size: 40px;
+}
+
+#view_bookings_past {
+  margin-top: 80px;
+  font-family: 'Outfit', sans-serif;
+  font-weight: 400;
+  font-size: 40px;
+}
+
+</style>
