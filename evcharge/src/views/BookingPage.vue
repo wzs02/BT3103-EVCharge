95ad6e194abfeaa2e649c0d4f75e7201da30fff9
qianyiloh <loh.qianyi@gmail.com>
2022-10-19 21:21:42 +0800

    Created "Book" page

---
+++ evcharge/src/views/BookingPage.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-app>
+    <NavBar />
+    <div>
+        <h1>Book a Charger</h1>
+    </div>
+
+  </v-app>
+</template>
+
+<script>
+import NavBar from "@/components/NavBar.vue";
+
+export default {
+    name: 'BookingPage',
+    components: { NavBar },
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Nunito&family=Outfit:wght@300&display=swap');
+</style>
