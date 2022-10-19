6af0c78eecf021271439ae6aaefb4457ca8b1cb2
qianyiloh <loh.qianyi@gmail.com>
2022-10-19 21:22:41 +0800

    Updated router to include "Book" page

--- evcharge/src/router/index.js
+++ evcharge/src/router/index.js
@@ -35,6 +35,11 @@ const router = createRouter({
 			component: () => import("../views/FAQPage.vue"),
 		},
 		{
+			path: "/Book",
+			name: "BookingPage",
+			component: () => import("../views/BookingPage.vue")
+		},
+		{
 			path: "/view_bookings",
 			name: "ViewBookings",
 			component: () => import("../views/ViewBookings.vue"),
