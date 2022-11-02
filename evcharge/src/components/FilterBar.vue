77090e3f8dcd9d583db662890bf3faa8d388bc56
qianyiloh <loh.qianyi@gmail.com>
2022-11-02 18:17:00 +0800

    Updated filter bar with multiselect

--- evcharge/src/components/FilterBar.vue
+++ evcharge/src/components/FilterBar.vue
@@ -4,16 +4,29 @@
 
             <v-col cols=4 class="filter">
                 <h4>Filter by location</h4>
-                <input v-model="searchQuery" placeholder="Filter by location">
-                <div v-for="r of resultQuery" :key="r.id">{{ r.title }}</div>
+                <VueMultiselect
+                    v-model="selected"
+                    :options="options"
+                    :multiple="true">
+                </VueMultiselect>
             </v-col>
 
             <v-col cols=4 class="filter">
                 <h4>Filter by charger type</h4>
+                <VueMultiselect
+                    v-model="selected"
+                    :options="options"
+                    :multiple="true">
+                </VueMultiselect>
             </v-col>
 
             <v-col cols=4 class="filter">
                 <h4>Filter by availability</h4>
+                <VueMultiselect
+                    v-model="selected"
+                    :options="options"
+                    :multiple="true">
+                </VueMultiselect>
             </v-col>
 
         </v-row>
@@ -28,46 +41,20 @@
 </template>
 
 <script>
+import VueMultiselect from 'vue-multiselect'
+
 export default {
     name: "FilterBar",
-    data() {
+    components: { VueMultiselect },
+    data () {
         return {
-            searchQuery: null,
-            resources: [
-                { id: 1, title: "javascript for dummies" },
-                { id: 2, title: "vue for dummies" },
-                { id: 3, title: "dos for dummies" },
-                { id: 4, title: "windows for dummies" },
-                { id: 5, title: "html for dummies" }    
-            ]
-        }
-    },
-    computed: {
-        resultQuery() {
-            if (this.searchQuery) {
-                return this.resources.filter(item => {
-                    return this.searchQuery
-                    .toLowerCase()
-                    .split("")
-                    .every(v => item.title.toLowerCase().includes(v))
-                })
-            } else {
-                return 0
-            }
+            selected: null,
+            options: ['Changi', 'Buona Vista', 'Kent Ridge']
         }
     }
 }
 </script>
 
-<style>
-
-.filter {
-    font-family: "Outfit";
-    font-style: normal;
-    font-weight: 400;
-    font-size: 18px;
-    padding-top: 5%;
-    padding-bottom: 5%
-}
+<style src="vue-multiselect/dist/vue-multiselect.css">
 
 </style>
