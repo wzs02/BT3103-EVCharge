f8b726e656a1d6b8039001055aa8bee4919a569d
qianyiloh <loh.qianyi@gmail.com>
2022-11-02 18:17:37 +0800

    Styling for booking page

--- evcharge/src/views/BookingPage.vue
+++ evcharge/src/views/BookingPage.vue
@@ -3,11 +3,11 @@
     <NavBar />
     <v-container>
 
-      <v-row>
-        <h1 class="header">Book a Charger</h1>
+      <v-row class="header">
+        <h1>Book a Charger</h1>
       </v-row>
 
-      <v-row>
+      <v-row class="filterbar">
         <FilterBar />
       </v-row>
 
@@ -17,13 +17,15 @@
         </v-col>
 
         <v-col cols=6>
-          <p>This where box be</p>
+          <div class="dayview">
+            <v-card color="#F5F5F5" height=500px>
+              <BookingCalendarDay />
+              <v-card-text>You are booking for </v-card-text>
+              <v-btn class="btn" rounded elevation="5">Book</v-btn>
+            </v-card>
+          </div>
         </v-col>
       </v-row>
-      
-        
-      
-       
      
     </v-container>
   </v-app>
@@ -33,10 +35,11 @@
 import NavBar from "@/components/NavBar.vue";
 import BookingCalendar from "@/components/BookingCalendar.vue"
 import FilterBar from "@/components/FilterBar.vue";
+import BookingCalendarDay from "@/components/BookingCalendarDay.vue"
 
 export default {
   name: 'BookingPage',
-  components: { NavBar, BookingCalendar, FilterBar },
+  components: { NavBar, BookingCalendar, FilterBar, BookingCalendarDay },
   data() {
     return {
       //
@@ -49,12 +52,36 @@ export default {
 @import url('https://fonts.googleapis.com/css2?family=Nunito&family=Outfit:wght@300&display=swap');
 
 .header {
-  padding-top: 100px;
-  padding-bottom: 20px;
+  margin: auto;
+  margin-top: 10%;
   font-family: "Outfit";
   font-style: normal;
   font-weight: 700;
-  font-size: 40px;
+  font-size: 20px;
+}
+
+.filterbar {
+  margin: auto;
+  margin-bottom: 5%;
+  font-family: "Outfit";
+  font-style: normal;
+  font-weight: 100;
+  font-size: 18px;
+}
+
+.dayview {
+  margin: auto;
+  font-family: "Outfit";
+  font-style: normal;
+  height: 50px;
+}
+
+.btn {
+    background-color: #4285f4;
+    color: #FFFFFF;
+    font-family: 'Outfit', sans-serif;
+    font-weight: bold;
+    border-radius: 20px;
 }
 
 </style>
