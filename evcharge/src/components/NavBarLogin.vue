8c91aa21d940da1c47e2f235474b6b7613bec0f6
laitzheng <75247468+laitzheng@users.noreply.github.com>
2022-11-05 16:31:09 +0800

    Update NavBar link

--- evcharge/src/components/NavBarLogin.vue
+++ evcharge/src/components/NavBarLogin.vue
@@ -11,7 +11,7 @@
 
                 <v-col cols="4" class="menu-options">
                     <button @click="$router.push('/')" class="menu-op">About</button>
-                    <button @click="$router.push('/TesterFile')" class="menu-op">Book</button>
+                    <button @click="$router.push('/Book')" class="menu-op">Book</button>
                     <button @click="$router.push('/TesterFile')" class="menu-op">Plan</button>
                     <v-icon style="font-size: 23px;">
                         mdi-bell-outline
