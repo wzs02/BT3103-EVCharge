333e683516ba4d969f33899f9d304c3252c0a250
qianyiloh <loh.qianyi@gmail.com>
2022-11-08 00:34:18 +0800

    Testing colour coding on calendar

--- evcharge/src/components/BookingCalendar.vue
+++ evcharge/src/components/BookingCalendar.vue
@@ -3,7 +3,8 @@
   </div>
   <v-calendar
     is-expanded
-    :attributes="attributes"
+    :attributes="attr"
+    :from-page="{ month: 11, year: 2022 }"
      />
   
 </template>
@@ -14,14 +15,56 @@ export default {
   name: 'BookingCalendar',
   components: { },
   data () {
+    const popup = [
+      {
+        description: "Available for booking",
+        isComplete: false,
+        dates: { weekdays:6 },
+        color: 'green',
+      }
+    ];
     return {
       date: new Date(),
-      attributes: [
+      attr: [
         {
           key: 'today',
-          highlight: 'blue',
+          highlight: {
+            color: 'blue',
+            fillMode: 'solid',
+          },
           dates: new Date(),
-        }
+        },
+        {
+          key: 'available',
+          highlight: {
+            color: 'green',
+            fillMode: 'solid',
+          },
+          dates: { start: new Date(2022, 10, 11), end: new Date(2022, 10, 15) },
+        },
+        {
+          key: 'unavailable',
+          highlight: {
+            color: 'red',
+            fillMode: 'solid',
+          },
+          dates: { start: new Date(2022, 10, 20), end: new Date(2022, 10, 24) },
+        },
+      ],
+      incId: popup.length,
+      popup,
+    }
+  },
+  computed: {
+    attributes() {
+      return [
+        this.popup.map(popupz => ({
+          dates: popupz.dates,
+          popover: {
+            label: popupz.description,
+          },
+          customData: popupz,
+        }))
       ]
     }
   }
