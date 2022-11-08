93ea9812642dca1bc636fa358e0418618cb481c0
qianyiloh <loh.qianyi@gmail.com>
2022-11-08 21:14:07 +0800

    Created "avail" and "unavail" attributes to reflect on calendar

--- evcharge/src/components/BookingCalendar.vue
+++ evcharge/src/components/BookingCalendar.vue
@@ -4,7 +4,7 @@
   <v-date-picker
     ref = "calendar"
     is-expanded
-    :attributes="attr"
+    :attributes="attributes"
      />
   
 </template>
@@ -23,30 +23,70 @@ export default {
   },
   data () {
     return {
-      attr: [
+      bookings: [
         {
-          key: 'available',
-          highlight: {
-            color: 'green',
-            fillMode: 'solid',
-          },
-          dates: [
-            { start: new Date(2022, 10, 11), end: new Date(2022, 10, 15) },
-            { start: new Date(2022, 10, 29), end: new Date(2022, 10, 30) },
-          ],
+          id: 1,
+          date: new Date(),
+          isAvailable: true,
         },
         {
-          key: 'unavailable',
-          highlight: {
-            color: 'red',
-            fillMode: 'solid',
-          },
-          dates: [],
+          id: 2,
+          date: new Date(2022, 10, 11),
+          isAvailable: false,
         },
       ],
+      // attr: [
+      //   {
+      //     key: 'available',
+      //     highlight: {
+      //       color: 'green',
+      //       fillMode: 'solid',
+      //     },
+      //     dates: [
+      //       { start: new Date(2022, 10, 11), end: new Date(2022, 10, 15) },
+      //       { start: new Date(2022, 10, 29), end: new Date(2022, 10, 30) },
+      //     ],
+      //   },
+      //   {
+      //     key: 'unavailable',
+      //     highlight: {
+      //       color: 'red',
+      //       fillMode: 'solid',
+      //     },
+      //     dates: [],
+      //   },
+      // ],
     }
   },
   computed: {
+    attributes() {
+      const unavail = this.bookings.filter(x => !x.isAvailable).map(x => ({
+        key: `booking.${x.id},`,
+        highlight: {
+          color: 'red',
+          fillMode: 'solid',
+        },
+        dates: x.date,
+        customData: x,
+      }))
+      const avail = this.bookings.filter(x => x.isAvailable).map(x => ({
+        key: `booking.${x.id},`,
+        highlight: {
+          color: 'green',
+          fillMode: 'solid',
+        },
+        dates: x.date,
+        customData: x,
+      }))
+      const arrayOfBookings = []
+      for (var i = 0; i <unavail.length; i++) {
+        arrayOfBookings.push(unavail[i])
+      }
+      for (var j = 0; j <avail.length; j++) {
+        arrayOfBookings.push(avail[j])
+      }
+      return arrayOfBookings
+    }
   },
   methods: {
     async getBookings() {
@@ -58,7 +98,7 @@ export default {
         let bookingValues = Object.values(bookingKeyValues)
         console.log(bookingAttributes)
         console.log(bookingValues)
-        this.attr.unavailable.dates = [{ start: new Date(2022,10,1), end: new Date(2022,10,2)}]
+        this.attr.unavailable.dates = [{ start: new Date(2022,10,1), end: new Date(2022,10,2) }]
       }
     }
   },
